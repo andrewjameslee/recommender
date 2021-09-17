31272da4f186edf4d9d68af49d0d9bb2a7e0155b
andrewjameslee <andrew@andrewjameslee.com>
2021-09-17 20:59:15 +1000

    feature added - admin interface

--- recommender/apicontroller.py
+++ recommender/apicontroller.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, jsonify, Blueprint,request,make_response
 from flask_restx import Api, Resource, reqparse
+from numpy import true_divide
 import recommender
 
 #initialise the API and namespace
@@ -17,15 +18,19 @@ api_extension = Api(apicontroller,
 name_space = api_extension.namespace('api', description='the data endpoint returns information on users interactions where the recommendation endpoint returns recommendations for the user')
 
 #setup parser for dataformat variable
-parser = reqparse.RequestParser()
-parser.add_argument('dataformat', type=str, help='can be set to "html". Any other value or left blank will return JSON')
+parserapi = reqparse.RequestParser()
+parserapi.add_argument('dataformat', type=str, help='can be set to "html". Any other value or left blank will return JSON')
+
 
 #setup URL routing
 @name_space.route("/v1/recommender/person/<int:person_id>")
+
 @name_space.doc(params={'person_id': 'id of person'})
-@name_space.doc(parser=parser)
+@name_space.doc(parser=parserapi)
 class RecommenderClass(Resource):
+
 	def get(self,person_id):
+            '''get recommendations for a person'''
             #check if variable dataformat exists and is set to html - if not set to none
             dataformat = request.args.get( 'dataformat',default=None)  
             #get recommendations from recommender 
@@ -41,10 +46,11 @@ class RecommenderClass(Resource):
 #setup URL routing
 @name_space.route("/v1/data/person/<int:person_id>")
 @name_space.doc(params={'person_id': 'id of person'})
-@name_space.doc(parser=parser)
+@name_space.doc(parser=parserapi)
 class StatsClass(Resource):
 
 	def get(self,person_id):
+            '''get interactions for a person'''        
 		    #check if variable dataformat exists and is set to html - if not set to none
             dataformat = request.args.get( 'dataformat',default=None)
             #get userstats from recommender 
@@ -58,3 +64,72 @@ class StatsClass(Resource):
                 return jsonify(results)
 
 
+admin_name_space = api_extension.namespace('admin', description='admin endpoint for administration functions')
+
+my_resource_parser = reqparse.RequestParser()
+my_resource_parser.add_argument('eventtype', type=str, default='')
+my_resource_parser.add_argument('eventstrength', type=float, default='')
+
+#setup URL routing
+@admin_name_space.route('/v1/admin/event/counts')
+class AdminEventClass(Resource):
+    #geteventstrength values
+    #puteventstrength values
+    #posteventstrengh values
+	def get(self):
+        
+            '''get event counts from dataset for all users by event type'''        
+            #get eventcounts from recommender 
+            results = recommender.recommender.dataworker().geteventcounts();
+
+            return results
+
+
+@admin_name_space.route('/v1/admin/event/eventstrength')
+@admin_name_space.expect(my_resource_parser)
+class AdminEventClass(Resource):
+	def get(self):
+            '''display event strength values'''        
+            #check if variable dataformat exists and is set to html - if not set to none
+            dataformat = request.args.get( 'dataformat',default=None)
+
+            #get eventcounts from recommender 
+            results = recommender.recommender.dataworker().getstatstrength();
+            #work out how to display the results
+            if dataformat == 'html':
+                response = make_response(results)
+                response.headers['Content-type'] = 'text/html'
+                return response;
+
+            else:
+                return results
+
+	def put(self):
+            '''modify event and strength'''   
+            eventtype = request.args.get( 'eventtype',default=None)
+            eventstrength = request.args.get( 'eventstrength',default=None)
+            
+            #check if variable dataformat exists and is set to html - if not set to none
+            dataformat = request.args.get( 'dataformat',default=None)
+
+            results = recommender.recommender.dataworker().getstatstrength();
+            if eventtype not in results:
+                return ('cannot modify '+ str(eventtype) + ' not in eventtypes ' + str(results) ) 
+            
+            #get eventcounts from recommender 
+            results = recommender.recommender.dataworker().updateevents(eventtype,eventstrength);
+            #work out how to display the results
+            if dataformat == 'html':
+                response = make_response(results)
+                response.headers['Content-type'] = 'text/html'
+                return response;
+
+            else:
+                return jsonify(results);                
+
+@admin_name_space.route('/v1/admin/recommender/regenerate')
+class AdminModelClass(Resource):
+	def post(self):
+            '''regenerate recommendations'''        
+            results = recommender.recommender.dataworker().regeneraterecommendation();
+            return results
--- recommender/recommender.py
+++ recommender/recommender.py
@@ -7,115 +7,123 @@ import implicit
 from sklearn.preprocessing import MinMaxScaler
 from sklearn import metrics
 
-
 import yaml
 import logging
 
-print("[RECOMMENDATION ENGINE] Initialising")
-with open('recommender/recommender-conf.yml') as stream:
-    config = yaml.safe_load(stream)
+grouped_df = ''
+df = ''
+event_type_strength = ''
+interactions_df = ''
+articles_df = ''
+def importdata():
+    print("[RECOMMENDATION ENGINE] Importing Data")
+    global grouped_df
+    global df
+    global event_type_strength
+    global interactions_df
+    global articles_df
+    articles_df = pd.read_csv(shared_articles_file)
+    interactions_df = pd.read_csv(users_interactions_file)
+    articles_df.drop(['authorUserAgent', 'authorRegion', 'authorCountry'], axis=1, inplace=True)
+    interactions_df.drop(['userAgent', 'userRegion', 'userCountry'], axis=1, inplace=True)
 
+    articles_df = articles_df[articles_df['eventType'] == 'CONTENT SHARED']
+    articles_df.drop('eventType', axis=1, inplace=True)
 
-DEBUG = config['DEBUG']
-shared_articles_file = config['shared_articles_file']
-users_interactions_file = config['users_interactions_file']
-
-
-if DEBUG == 'True':
-    print('DEBUG set to', DEBUG)
-
-print("[RECOMMENDATION ENGINE] Importing Data")
-articles_df = pd.read_csv(shared_articles_file)
-interactions_df = pd.read_csv(users_interactions_file)
-articles_df.drop(['authorUserAgent', 'authorRegion', 'authorCountry'], axis=1, inplace=True)
-interactions_df.drop(['userAgent', 'userRegion', 'userCountry'], axis=1, inplace=True)
-
-#debugging
-if DEBUG == 'True':
-    print(articles_df['eventType'].value_counts())
-
-articles_df = articles_df[articles_df['eventType'] == 'CONTENT SHARED']
-articles_df.drop('eventType', axis=1, inplace=True)
-
-#debugging
-if DEBUG == 'True':
-    print(articles_df.info())
-    print(interactions_df.info())
-
-df = pd.merge(interactions_df[['contentId','personId', 'eventType']], articles_df[['contentId', 'title','url']], how = 'inner', on = 'contentId')
-
-
-
-#debugging
-if DEBUG == 'True':
-    print(df.head(10))
-    print(df.info())
-    print(df['eventType'].value_counts())
     
-event_type_strength = {
-   'VIEW': 1.0,
-   'LIKE': 2.0, 
-   'BOOKMARK': 3.0, 
-   'FOLLOW': 2.0,
-   'COMMENT CREATED': 2.0,  
-   'DISLIKE': -10.0,
-}
-
-df['eventStrength'] = df['eventType'].apply(lambda x: event_type_strength[x])
-
-#debugging
-if DEBUG == 'True':
-    print(df.head(10))
-
-df = df.drop_duplicates()
-grouped_df = df.groupby(['personId', 'contentId', 'title','url']).sum().reset_index()
+    event_type_strength = {
+    'VIEW': 1.0,
+    'LIKE': 2.0, 
+    'BOOKMARK': 3.0, 
+    'FOLLOW': 2.0,
+    'COMMENT CREATED': 2.0,  
+    'DISLIKE': -10.0,
+    }
+    
 
 
+sparse_content_person = ''
+sparse_person_content = ''
+person_vecs = ''
+content_vecs = ''
+similar = ''
 
-#debugging
-if DEBUG == 'True':
-    print(grouped_df.sample(10))
-    print(grouped_df.dtypes)
+def recommendcore():
+    print("[RECOMMENDATION ENGINE] Building model")
+    global df
+    global grouped_df
+    global sparse_content_person
+    global sparse_person_content
+    global person_vecs
+    global content_vecs
+    global similar
+    global event_type_strength
+    df = pd.merge(interactions_df[['contentId','personId', 'eventType']], articles_df[['contentId', 'title','url']], how = 'inner', on = 'contentId')
 
-grouped_df['title'] = grouped_df['title'].astype("category")
-grouped_df['personId'] = grouped_df['personId'].astype("category")
-grouped_df['contentId'] = grouped_df['contentId'].astype("category")
-grouped_df['person_id'] = grouped_df['personId'].cat.codes
-grouped_df['content_id'] = grouped_df['contentId'].cat.codes
+    df['eventStrength'] = df['eventType'].apply(lambda x: event_type_strength[x])
 
+    #debugging
+    if DEBUG == 'True':
+        print(df.head(10))
+        print(df.info())
+        print(df['eventType'].value_counts())
+        
+    #debugging
+    if DEBUG == 'True':
+        print(df.head(10))
 
+    df = df.drop_duplicates()
+    grouped_df = df.groupby(['personId', 'contentId', 'title','url']).sum().reset_index()
 
+    #debugging
+    if DEBUG == 'True':
+        print(grouped_df.sample(10))
+        print(grouped_df.dtypes)
 
+    grouped_df['title'] = grouped_df['title'].astype("category")
+    grouped_df['personId'] = grouped_df['personId'].astype("category")
+    grouped_df['contentId'] = grouped_df['contentId'].astype("category")
+    grouped_df['person_id'] = grouped_df['personId'].cat.codes
+    grouped_df['content_id'] = grouped_df['contentId'].cat.codes
 
-print("[RECOMMENDATION ENGINE] Building model")
-sparse_content_person = sparse.csr_matrix((grouped_df['eventStrength'].astype(float), (grouped_df['content_id'], grouped_df['person_id'])))
-sparse_person_content = sparse.csr_matrix((grouped_df['eventStrength'].astype(float), (grouped_df['person_id'], grouped_df['content_id'])))
+    sparse_content_person = sparse.csr_matrix((grouped_df['eventStrength'].astype(float), (grouped_df['content_id'], grouped_df['person_id'])))
+    sparse_person_content = sparse.csr_matrix((grouped_df['eventStrength'].astype(float), (grouped_df['person_id'], grouped_df['content_id'])))
 
+    model = implicit.als.AlternatingLeastSquares(factors=20, regularization=0.1, iterations=50)
+    alpha = 15
+    data = (sparse_content_person * alpha).astype('double')
 
-model = implicit.als.AlternatingLeastSquares(factors=20, regularization=0.1, iterations=50)
-alpha = 15
-data = (sparse_content_person * alpha).astype('double')
+    # Fit the model
+    print("[RECOMMENDATION ENGINE] Fitting model to data")
+    model.fit(data)
 
-# Fit the model
-print("[RECOMMENDATION ENGINE] Fitting model to data")
-model.fit(data)
+    content_id = 450
+    n_similar = 10
 
-content_id = 450
-n_similar = 10
+    person_vecs = model.user_factors
+    content_vecs = model.item_factors
 
-person_vecs = model.user_factors
-content_vecs = model.item_factors
+    content_norms = np.sqrt((content_vecs * content_vecs).sum(axis=1))
 
-content_norms = np.sqrt((content_vecs * content_vecs).sum(axis=1))
-
-scores = content_vecs.dot(content_vecs[content_id]) / content_norms
-top_idx = np.argpartition(scores, -n_similar)[-n_similar:]
-similar = sorted(zip(top_idx, scores[top_idx] / content_norms[content_id]), key=lambda x: -x[1])
+    scores = content_vecs.dot(content_vecs[content_id]) / content_norms
+    top_idx = np.argpartition(scores, -n_similar)[-n_similar:]
+    similar = sorted(zip(top_idx, scores[top_idx] / content_norms[content_id]), key=lambda x: -x[1])
 
     # Get the trained person and content vectors. We convert them to csr matrices
 
-person_vecs = sparse.csr_matrix(model.user_factors)
-content_vecs = sparse.csr_matrix(model.item_factors)
+    person_vecs = sparse.csr_matrix(model.user_factors)
+    content_vecs = sparse.csr_matrix(model.item_factors)
+
+
+
+
+
+def eventcounts():
+    return(df['eventType'].value_counts())
+
+def eventstrength():
+    print (event_type_strength)
+    return(event_type_strength)
 
 def recommend(person_id, sparse_person_content, person_vecs, content_vecs, num_contents=10):
     try:   
@@ -153,9 +161,11 @@ def recommend(person_id, sparse_person_content, person_vecs, content_vecs, num_c
     except:
             return ("an error has occured")
 
-import random
+
+
 
 def make_train(ratings, pct_test = 0.2):
+    import random
     test_set = ratings.copy() # Make a copy of the original set to be the test set. 
     test_set[test_set != 0] = 1 # Store the test set as a binary preference matrix
     
@@ -180,65 +190,8 @@ def make_train(ratings, pct_test = 0.2):
     
     return training_set, test_set, list(set(person_inds))
 
-content_train, content_test, content_persons_altered = make_train(sparse_content_person, pct_test = 0.2)
+    content_train, content_test, content_persons_altered = make_train(sparse_content_person, pct_test = 0.2)
 
-
-if DEBUG == 'True':
-    print('similar content')
-    for content in similar:
-        idx, score = content
-        print(grouped_df.title.loc[grouped_df.content_id == idx].iloc[0])
-
-class dataworker:
-
-    def getuserstats(self, person_id,dataformat=None):
-        try:
-            #lookup personid64 from personid
-            person_id64 = grouped_df.personId.loc[grouped_df['person_id'] == person_id ].iloc[0]
-            #get stats from personid
-            stats = df.loc[df['personId'] == person_id64 ]
-        
-            if dataformat == 'html':
-                return stats.to_html()
-            else:
-                return stats.to_json(orient ='index')
-        except (IndexError) as e:
-            print('EXCEPTION RAISED: ' + str(e))
-            print(e.args)
-            return ('cannot return data stats for person_id ' + str(person_id) )     
-          
-
-    def getrecommend(self, person_id,dataformat=None):
-        try:
-            recommendations = recommend(person_id, sparse_person_content, person_vecs, content_vecs)
-            if dataformat == 'html':
-                return recommendations.to_html()
-            else:
-                return recommendations.to_json(orient ='index')
-        
-        except (IndexError, AttributeError) as e:
-            print('EXCEPTION RAISED: ' + str(e))
-            print(e.args)
-            return ('cannot return recommendations for person_id ' + str(person_id) ) 
-
-
-
-#debugging
-if DEBUG == 'True':
-    print(grouped_df.head(10))
-    print("===afterthis===")
-    person_id = 0
-    userstatistics = recommender.userstats(person_id)
-    print(userstatistics)
-    print("===beforethis===")
-
-#debugging
-if DEBUG == 'True':
-    person_id = 1
-    recommendations = recommender.recommend(person_id, sparse_person_content, person_vecs, content_vecs)
-    print('recommendations for', person_id)
-    print(recommendations)
-    print(grouped_df.loc[grouped_df['person_id'] == 1].sort_values(by=['eventStrength'], ascending=False)[['title', 'url', 'eventStrength', 'person_id']])
 
 
 
@@ -274,9 +227,118 @@ def calc_mean_auc(training_set, altered_persons, predictions, test_set):
     
     return float('%.3f'%np.mean(store_auc)), float('%.3f'%np.mean(popularity_auc))   
 
+
+
+class dataworker:
+
+    def getuserstats(self, person_id,dataformat=None):
+        try:
+            #lookup personid64 from personid
+            person_id64 = grouped_df.personId.loc[grouped_df['person_id'] == person_id ].iloc[0]
+            #get stats from personid
+            stats = df.loc[df['personId'] == person_id64 ]
+        
+            if dataformat == 'html':
+                return stats.to_html()
+            else:
+                return stats.to_json(orient ='index')
+        except (IndexError) as e:
+            print('EXCEPTION RAISED: ' + str(e))
+            print(e.args)
+            return ('cannot return data stats for person_id ' + str(person_id) )     
+          
+
+    def getrecommend(self, person_id,dataformat=None):
+        try:
+            recommendations = recommend(person_id, sparse_person_content, person_vecs, content_vecs)
+            if dataformat == 'html':
+                return recommendations.to_html()
+            else:
+                return recommendations.to_json(orient ='index')
+        
+        except (IndexError, AttributeError) as e:
+            print('EXCEPTION RAISED: ' + str(e))
+            print(e.args)
+            return ('cannot return recommendations for person_id ' + str(person_id) ) 
+
+    def getstatstrength(self):
+        try:
+            stats = eventstrength()
+            return stats
+        
+        except (IndexError, AttributeError) as e:
+            print('EXCEPTION RAISED: ' + str(e))
+            print(e.args)
+            return ('cannot return eventstrength' ) 
+
+    def updateevents(self,eventtype,eventstrength):
+        try:
+            global event_type_strength
+            event_type_strength[eventtype] = float(eventstrength)
+            return event_type_strength
+        
+        except (IndexError, AttributeError) as e:
+            print('EXCEPTION RAISED: ' + str(e))
+            print(e.args)
+            return ('cannot update event type and strength' ) 
+
+    def geteventcounts(self):
+        try:
+            stats = eventcounts()
+            return stats.to_json(orient ='index')
+        
+        except (IndexError, AttributeError) as e:
+            print('EXCEPTION RAISED: ' + str(e))
+            print(e.args)
+            return ('cannot return eventcounts' )
+
+    def regeneraterecommendation(self):
+        try:
+            results = recommendcore()
+            return  ('recommendations have been regenerated' )
+        
+        except (IndexError, AttributeError) as e:
+            print('EXCEPTION RAISED: ' + str(e))
+            print(e.args)
+            return ('cannot regenerate recommendations' )
+
+print("[RECOMMENDATION ENGINE] Initialising")
+with open('recommender/recommender-conf.yml') as stream:
+    config = yaml.safe_load(stream)
+
+DEBUG = config['DEBUG']
+shared_articles_file = config['shared_articles_file']
+users_interactions_file = config['users_interactions_file']
+
+if DEBUG == 'True':
+    print('DEBUG set to', DEBUG)
+
+importdata()
+recommendcore()
+
+#debugging
+if DEBUG == 'True':
+    person_id = 1
+    recommendations = recommend(person_id, sparse_person_content, person_vecs, content_vecs)
+    print('recommendations for', person_id)
+    print(recommendations)
+    print(grouped_df.loc[grouped_df['person_id'] == 1].sort_values(by=['eventStrength'], ascending=False)[['title', 'url', 'eventStrength', 'person_id']])
+
+if DEBUG == 'True':
+    print('similar content')
+    for content in similar:
+        idx, score = content
+        print(grouped_df.title.loc[grouped_df.content_id == idx].iloc[0])
+        
 if DEBUG == 'True':
     print("[RECOMMENDATION ENGINE] store_auc and popularity_auc are", calc_mean_auc(content_train, content_persons_altered,
               [person_vecs, content_vecs.T], content_test))
 
+    #debugging
+    if DEBUG == 'True':
+        print(articles_df['eventType'].value_counts())
 
-              
+    #debugging
+    if DEBUG == 'True':
+        print(articles_df.info())
+        print(interactions_df.info())        
